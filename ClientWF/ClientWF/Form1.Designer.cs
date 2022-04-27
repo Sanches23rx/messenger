1e442f7e9fb21b3c1314c1e4f205edf7b1847280
Sanches23rx <89974705+Sanches23rx@users.noreply.github.com>
2022-04-27 11:05:11 +0300

    Add files via upload

---
+++ ClientWF/ClientWF/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace ClientWF
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.loginTB = new System.Windows.Forms.TextBox();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(768, 344);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 363);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 409);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(668, 27);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Развития и сил Вам ребята;)";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 566);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // loginTB
+            // 
+            this.loginTB.Location = new System.Drawing.Point(20, 521);
+            this.loginTB.Name = "loginTB";
+            this.loginTB.Size = new System.Drawing.Size(192, 27);
+            this.loginTB.TabIndex = 6;
+            this.loginTB.Text = "rusal";
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(20, 568);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(192, 27);
+            this.passwordTB.TabIndex = 7;
+            this.passwordTB.Text = "1234";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Registration";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(597, 566);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 24);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 607);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.loginTB);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox loginTB;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ ClientWF/ClientWF/Form1.cs
@@ -0,0 +1,176 @@
+﻿using ClassLib;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWF
+{
+  public partial class Form1 : Form
+  {
+    public int pos = 0;
+    public string baseUrl = "http://localhost:5000";
+    public string token;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      string res = "";
+      while (res != "Not found")
+      {
+        var client = new RestClient(baseUrl);
+        var request = new RestRequest("api/GetMessage/" + pos, Method.GET);
+        var queryResult = client.Execute(request);
+        res = queryResult.Content;
+        res = res.Trim('\"');
+        if (res != "Not found")
+        {
+          listBox1.Items.Add(res);
+          pos++;
+        }
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      var client = new RestClient(baseUrl);
+      var request = new RestRequest("api/sendmessage", Method.POST);
+      request.RequestFormat = DataFormat.Json;
+      ClassLib.MessageClass mes = new ClassLib.MessageClass();
+      mes.userName = textBox1.Text;
+      mes.messageText = textBox2.Text;
+      mes.timeStamp = DateTime.Now.ToString();
+      mes.token = token;
+      request.AddBody(mes);
+      client.Execute(request);
+    }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string url = baseUrl + "/api/login";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                LoginClass lg = new LoginClass();
+                lg.login = loginTB.Text.ToLower();
+                lg.password = CryptClass.GetSHA256(passwordTB.Text);
+                string jsonString = JsonConvert.SerializeObject(lg, Formatting.Indented);
+                streamWriter.Write(jsonString);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string strdata = "";
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                strdata = streamReader.ReadToEnd();
+            }
+
+            token = strdata;
+
+            //getLoginByToken
+            string url2 = baseUrl + "/api/getLoginByToken";
+            var httpWebRequest2 = (HttpWebRequest)WebRequest.Create(url2);
+            httpWebRequest2.ContentType = "application/json";
+            httpWebRequest2.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest2.GetRequestStream()))
+            {
+                streamWriter.Write("\""+token+ "\"");
+            }
+
+            var httpResponse2 = (HttpWebResponse)httpWebRequest2.GetResponse();
+            string strdata2 = "";
+            using (var streamReader = new StreamReader(httpResponse2.GetResponseStream()))
+            {
+                strdata2 = streamReader.ReadToEnd();
+            }
+
+            textBox1.Text = strdata2;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = baseUrl + "/api/reg";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                LoginClass lg = new LoginClass();
+                lg.login = loginTB.Text.ToLower();
+                lg.password = CryptClass.GetSHA256(passwordTB.Text);
+                string jsonString = JsonConvert.SerializeObject(lg, Formatting.Indented);
+                streamWriter.Write(jsonString);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string strdata = "";
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                strdata = streamReader.ReadToEnd();
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index > 0)
+            {
+                textBox2.Text= "цитата: ^"+listBox1.Items[index].ToString()+"^";
+            }
+            
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //if (checkBox1.Checked)
+            //{
+            //    MessageBox.Show("Bla");
+            //}
+
+
+        }
+
+        private void checkBox1_CheckStateChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                this.BackColor = Color.Black;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+        }
+    }
+}
